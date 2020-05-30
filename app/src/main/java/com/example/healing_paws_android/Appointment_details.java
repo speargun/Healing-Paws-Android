edc0b40531bed10a26ea2d48c0653f3d32abc9b2
yangyang.zhang <55621892+ZhangYangyang17205983@users.noreply.github.com>
2020-05-31 06:41:08 +0800

    .

--- app/src/main/java/com/example/healing_paws_android/Appointment_details.java
+++ app/src/main/java/com/example/healing_paws_android/Appointment_details.java
@@ -2,6 +2,7 @@ package com.example.healing_paws_android;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -9,84 +10,145 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class Appointment_details extends AppCompatActivity {
-    private ListView mListView;
+
+    private TextView pet = findViewById(R.id.pet);
+    private TextView location = findViewById(R.id.location);
+
+    private TextView change = findViewById(R.id.change);
+    private TextView doctor = findViewById(R.id.doctor);
+    private TextView description = findViewById(R.id.description);
+    private TextView type = findViewById(R.id.type);
+    private TextView time = findViewById(R.id.time);
+
+    private Button back = findViewById(R.id.back);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment_details);
-        mListView=(ListView)findViewById(R.id.lv);
-        List<appointmentdata> mappointmentdataList=new ArrayList<>();
-        for( int i = 1; i <= 5 ; i++) {
-            appointmentdata mappointmentData = new appointmentdata();
-            mappointmentData.setDname("doctor: " + mappointmentData.getDname());
-            mappointmentData.setAddress("address:"+ mappointmentData.getAddress());
-            mappointmentData.setTime("time:"+ mappointmentData.getTime());
-            mappointmentdataList.add(mappointmentData);
+        Intent intent = getIntent();
+        String atime = intent.getStringExtra("atime");
+        time.setText("Datetime:"+atime);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Appointment_details.this,Check_appointment.class);
+
+                startActivity(intent);
+            }
+        });
+
+        try {
+            details();
+        }catch(SQLException e){
+            String sql_error = getString(R.string.sql_error);
+            Toast.makeText(Appointment_details.this,sql_error,Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
         }
 
-
-
-
-
-
-        mListView.setAdapter(new Appointment_details.MyBaseAdapter(mappointmentdataList,this));
 
 
     }
-    class MyBaseAdapter extends BaseAdapter{
-        private List<appointmentdata> mappointmentdataList;
-        private LayoutInflater inflater;
 
-        public  MyBaseAdapter (List<appointmentdata> mappointmentdataList, Context context) {
-            this.mappointmentdataList = mappointmentdataList;
-            this.inflater = LayoutInflater.from(context);
+
+
+
+
+    public void details() throws SQLException{
+        // 注册驱动
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            //注册失败
+            e.printStackTrace();
         }
-        @Override
-        public int getCount() {
-            return mappointmentdataList == null?0:mappointmentdataList.size();  //判断有说个Item
-        }
+        // 获得链接
+        Connection conn = DriverManager.getConnection("jdbc:mysql://47.98.48.168:3306", "root", "root");
+        // 得到操作数据库sql语句的对象Statement
+        Statement st = conn.createStatement();
+        SharedPreferences sharedPreferences = getSharedPreferences("Username", MODE_PRIVATE);
+        String username = sharedPreferences.getString("username",null);
 
-        @Override
-        public Object getItem(int position) {
-            return mappointmentdataList.get(position);
-        }
+        st.executeQuery("use test");
 
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
+        ResultSet rs = st.executeQuery("SELECT DISTINCT\n" +
+                "    name\n" +
+                "FROM\n" +
+                "    pets AS p,\n" +
+                "    appointments AS a\n" +
+                "users AS u\n"+
+                "WHERE\n" +
+                "    p.id = a.pet_id AND  p.owner_id = u.id AND u.username = '"+username+"AND a.datetime = '"+time+"';");
+       pet.setText("Pet:"+rs.toString());
+        rs = st.executeQuery("SELECT \n" +
+                "    loc\n" +
+                "FROM\n" +
+                "    pets AS p,\n" +
+                "    appointments AS a\n" +
+                "users AS u\n"+
+                "WHERE\n" +
+                "    p.id = a.pet_id AND  p.owner_id = u.id AND  u.username = '"+username+"AND a.datetime = '"+time+"';");
+        location.setText("Location:"+rs.toString());
+        rs = st.executeQuery("SELECT \n" +
+                "    changeable\n" +
+                "FROM\n" +
+                "    pets AS p,\n" +
+                "    appointments AS a\n" +
+                "users AS u\n"+
+                "WHERE\n" +
+                "    p.id = a.pet_id AND  p.owner_id = u.id AND  u.username = '"+username+"AND a.datetime = '"+time+"';");
+        change.setText(rs.toString());
+        rs = st.executeQuery("SELECT \n" +
+                "    preferred_doctor_id\n" +
+                "FROM\n" +
+                "    pets AS p,\n" +
+                "    appointments AS a\n" +
+                "users AS u\n"+
+                "WHERE\n" +
+                "    p.id = a.pet_id AND  p.owner_id = u.id AND u.username = '"+username+"AND a.datetime = '"+time+"';");
+       doctor.setText("Doctor:"+rs.toString());
+        rs = st.executeQuery("SELECT \n" +
+                "    description\n" +
+                "FROM\n" +
+                "    pets AS p,\n" +
+                "    appointments AS a\n" +
+                "users AS u\n"+
+                "WHERE\n" +
+                "    p.id = a.pet_id AND  p.owner_id = u.id AND u.username = '"+username+"AND a.datetime = '"+time+"';");
+        description.setText("Description:"+rs.toString());
 
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            //加载布局为一个视图
-            View view = inflater.inflate(R.layout.activity_appointment_details,null);
-            appointmentdata mappointmentdata = (appointmentdata) getItem(position);
-
-            //在view 视图中查找 组件
-            TextView tv_dname = (TextView) view.findViewById(R.id.text_dname);
-            TextView tv_address = (TextView) view.findViewById(R.id.text_address);
-            TextView tv_time = (TextView) view.findViewById(R.id.text_time);
-
-            //为Item 里面的组件设置相应的数据
-            tv_dname.setText("doctor:"+mappointmentdata.getDname());
-            tv_address.setText("address: "+ mappointmentdata.getAddress());
-
-            tv_time.setText("time: "+ mappointmentdata.getTime());
-            //返回含有数据的view
-            return view;
-        }
+        rs = st.executeQuery("SELECT \n" +
+                "    is_emergency\n" +
+                "FROM\n" +
+                "    pets AS p,\n" +
+                "    appointments AS a\n" +
+                "users AS u\n"+
+                "WHERE\n" +
+                "    p.id = a.pet_id AND  p.owner_id = u.id AND u.username = '"+username+"AND a.datetime = '"+time+"';");
+        type.setText("Type:"+rs.toString());
+        rs.close();
+        st.close();
+        conn.close();
 
     }
-
 
 }
--- app/src/main/java/com/example/healing_paws_android/Check_appointment.java
+++ app/src/main/java/com/example/healing_paws_android/Check_appointment.java
@@ -2,6 +2,7 @@ package com.example.healing_paws_android;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,28 +11,100 @@ import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Driver;
+import java.util.Calendar;
+import java.util.Map;
+
+import static android.view.View.inflate;
+
+
+
+
 public class Check_appointment extends AppCompatActivity {
+
     private ListView mListView;
 
+    final ArrayList<String> petsList = new ArrayList<>();
+
+    final ArrayList<String> timeList = new ArrayList<>();
+
+
+    final String[] pets = new String[petsList.size()];
+
+    final String[] time = new String[petsList.size()];
+
+
+
+    //  private int[] icons={R.drawable.jd,R.drawable.tmall,R.drawable.sina,R.drawable.qq_dizhu,R.drawable.qq,R.drawable.uc};
     @Override
+
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_appointment);
-        mListView=(ListView)findViewById(R.id.lv);
-        List<appointmentdata> mappointmentdataList=new ArrayList<>();
-        for( int i = 1; i <= 5 ; i++) {
-            appointmentdata mappointmentData = new appointmentdata();
-            mappointmentData.setDname("doctor: " + mappointmentData.getDname());
-            mappointmentData.setAddress("address:"+ mappointmentData.getAddress());
 
-            mappointmentdataList.add(mappointmentData);
+
+
+
+
+        try {
+            acquire_info(petsList,timeList);
+        }catch(SQLException e){
+            String sql_error = getString(R.string.sql_error);
+            Toast.makeText(Check_appointment.this,sql_error,Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+
+        int i = 0;
+        while(i<petsList.size()){
+            pets[i] = petsList.get(i);
+
+            i++;
+        }
+
+        i = 0;
+        while(i<timeList.size()){
+            time[i] = timeList.get(i);
+
+            i++;
         }
 
 
@@ -39,58 +112,127 @@ public class Check_appointment extends AppCompatActivity {
 
 
 
-        mListView.setAdapter(new MyBaseAdapter(mappointmentdataList,this));
+
+
+
+
+
+
+
+        mListView=(ListView)findViewById(R.id.lv);
+        mListView.setAdapter(new MyBaseAdapter());
+
+
+
 
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = null;
+                Intent intent = new Intent();
+                TextView atime = (TextView) view.findViewById(R.id.tv_list);
+
+                intent.putExtra("atime",atime.getText().toString());
+
                 intent = new Intent(Check_appointment.this,Appointment_details.class);
                 startActivity(intent);
             }
         });
-    }
-    class MyBaseAdapter extends BaseAdapter{
-        private List<appointmentdata> mappointmentdataList;
-        private LayoutInflater inflater;
 
-        public  MyBaseAdapter (List<appointmentdata> mappointmentdataList, Context context) {
-            this.mappointmentdataList = mappointmentdataList;
-            this.inflater = LayoutInflater.from(context);
-        }
+
+
+
+
+    }
+
+
+
+
+
+    class MyBaseAdapter extends BaseAdapter{
+
         @Override
         public int getCount() {
-            return mappointmentdataList == null?0:mappointmentdataList.size();  //判断有说个Item
+            return time.length;
         }
-
         @Override
         public Object getItem(int position) {
-            return mappointmentdataList.get(position);
+            return time [position];
         }
-
         @Override
         public long getItemId(int position) {
             return position;
         }
-
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            //加载布局为一个视图
-            View view = inflater.inflate(R.layout.activity_check_appointment,null);
-            appointmentdata mappointmentdata = (appointmentdata) getItem(position);
-
-            //在view 视图中查找 组件
-            TextView tv_dname = (TextView) view.findViewById(R.id.text_dname);
-            TextView tv_address= (TextView) view.findViewById(R.id.text_address);
-
-
-            //为Item 里面的组件设置相应的数据
-            tv_dname.setText("doctor:"+mappointmentdata.getDname());
-            tv_address.setText("address: "+ mappointmentdata.getAddress());
-
-            //返回含有数据的view
+        public View getView(int position, View convertView, ViewGroup parent) {//组装数据
+            View view=View.inflate(Check_appointment.this,R.layout.list_item,null);//在list_item中有两个id,现在要把他们拿过来
+            TextView mTextView=(TextView) view.findViewById(R.id.tv_list);
+            //ImageView imageView=(ImageView)view.findViewById(R.id.image);
+//组件一拿到，开始组装
+            mTextView.setText(time[position]);
+            //imageView.setBackgroundResource(icons[position]);
+//组装玩开始返回
             return view;
+
         }
+
+
+
+    }
+
+
+
+
+
+
+
+    public void acquire_info(ArrayList<String> pets,ArrayList<String> time) throws SQLException {
+
+        // 注册驱动
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            //注册失败
+            e.printStackTrace();
+        }
+        // 获得链接
+        Connection conn = DriverManager.getConnection("jdbc:mysql://47.98.48.168:3306", "root", "root");
+        // 得到操作数据库sql语句的对象Statement
+        Statement st = conn.createStatement();
+        SharedPreferences sharedPreferences = getSharedPreferences("Username", MODE_PRIVATE);
+        String username = sharedPreferences.getString("username",null);
+        // 执行
+        st.executeQuery("use test");
+        ResultSet rs = st.executeQuery("SELECT \n" +
+                "    name\n" +
+                "FROM\n" +
+                "    pets AS p,\n" +
+                "    users AS u\n" +
+                "WHERE\n" +
+                "    p.owner_id = u.id AND has datetime AND u.username = '"+username+"';");
+        while (rs.next()) {
+            pets.add(rs.getString(1));
+        }
+        rs.close();
+
+        ResultSet rs1 = st.executeQuery("SELECT \n" +
+                "    datetime\n" +
+                "FROM\n" +
+               "appointments As a\n"+
+                "    pets AS p,\n" +
+                "    users AS u\n" +
+                "WHERE\n" +
+                " a.pet_id = p.id AND p.owner_id = u.id AND u.username = '"+username+"';");
+        while (rs1.next()) {
+//            System.out.println(rs1.getString(1));
+            time.add(rs1.getString(1));
+        }
+        // 关闭资源
+        rs1.close();
+
+
+
+        st.close();
+        conn.close();
 
     }
 
